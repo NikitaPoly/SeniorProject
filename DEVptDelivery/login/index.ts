1351e7f2a17f6a925d6ca481ddd6f188444ccb2b
nikitapoly <nikitapoly@comcast.net>
2022-02-28 16:37:34 -0500

    added three.js to project, added default animation set up to log in

--- DEVptDelivery/login/index.ts
+++ DEVptDelivery/login/index.ts
@@ -7,6 +7,8 @@ import navStyle from "../modules/css/nav.css";
 import footerStyle from "../modules/css/footer.css";
 import mainStyle from "../modules/css/login.css"
 
+import * as THREE from "three";
+
 console.log(defaultStyle, navStyle, footerStyle, mainStyle);//activate styles for page
 defaults.defaults();
 navfuncs.setActive("login");
@@ -39,7 +41,7 @@ main.appendChild(span);
 let animationDiv: HTMLElement = document.createElement("div");
 animationDiv.id = "animationDiv";
 //three.js canvas elements
-let canvas: HTMLElement = document.createElement("canvas");
+let canvas: HTMLCanvasElement = document.createElement("canvas");
 //the content of animation overlay
 let p: HTMLElement = document.createElement("p");
 p.innerHTML = "Often students on college campuses seek out employment opportunities that do not interfere with a busy class schedule. Some students appreciate the convenience of ordering food and having it delivered. The campus delivery system gives busy students an opportunity to gain flexible employment while also providing cheap and easy delivery for on campus stores. "
@@ -60,4 +62,93 @@ for (let i = 0; i < animationLayover.length; i++) {
 animationDiv.appendChild(canvas)
 animationDiv.appendChild(overLayDiv);
 main.appendChild(animationDiv)
+//structure set up is done here
+//this part of the code is responsible for the animation for sign up
+function signUpAnimation() {// responsible to run the user thru the account creation steps
+    //basic three.js setup
+    const scene: THREE.Scene = new THREE.Scene();
+    const fov: number = 75;
+    const aspect: number = 2;  // the canvas default
+    const near: number = 0.1;
+    const far: number = 5;
+    const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    //move camera back
+    camera.position.z = 2;
+    //connect our canvs to the three.js engine
+    const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ canvas });
+    renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+    //make the menu and description disappear
+    p.style.display = "none";
+    buttonLogin.style.display = "none";
+    buttonSignup.style.display = "none"
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    function myRender(time: number) {//custome render function for animations
+        time *= 0.001;
+        let widthAnimationDiv: string = animationDiv.style.width;
+        let heightAnimationDiv: string = animationDiv.style.height;
+        //get the three.js representation of the canvas and
+        const test = renderer.domElement;
+        camera.aspect = test.clientWidth / test.clientHeight;
+        camera.updateProjectionMatrix();
+        canvas.style.width = widthAnimationDiv;
+        canvas.style.height = heightAnimationDiv;
 
+        //update the aspect ratio
+        cube.rotation.x = -time;
+        cube.rotation.y = time;
+        renderer.render(scene, camera);
+        requestAnimationFrame(myRender);
+    }
+    requestAnimationFrame(myRender);
+}
+function loginAnimation(){//login animation
+//basic three.js setup
+const scene: THREE.Scene = new THREE.Scene();
+const fov: number = 75;
+const aspect: number = 2;  // the canvas default
+const near: number = 0.1;
+const far: number = 5;
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+//move camera back
+camera.position.z = 2;
+//connect our canvs to the three.js engine
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+//make the menu and description disappear
+p.style.display = "none";
+buttonLogin.style.display = "none";
+buttonSignup.style.display = "none"
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+function myRender(time: number) {//custome render function for animations
+    time *= 0.001;
+    let widthAnimationDiv: string = animationDiv.style.width;
+    let heightAnimationDiv: string = animationDiv.style.height;
+    //get the three.js representation of the canvas and
+    const test = renderer.domElement;
+    camera.aspect = test.clientWidth / test.clientHeight;
+    camera.updateProjectionMatrix();
+    canvas.style.width = widthAnimationDiv;
+    canvas.style.height = heightAnimationDiv;
+
+    //update the aspect ratio
+    cube.rotation.x = -time;
+    cube.rotation.y = time;
+    renderer.render(scene, camera);
+    requestAnimationFrame(myRender);
+}
+requestAnimationFrame(myRender);
+}
+buttonLogin.addEventListener("click",loginAnimation);
+buttonSignup.addEventListener("click", signUpAnimation);
