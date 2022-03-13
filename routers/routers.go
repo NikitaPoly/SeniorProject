ca35441c4746e1a49b8c8a1410d69d5cf0444a2d
nikitapoly <nikitapoly@comcast.net>
2022-03-12 19:48:54 -0500

    added get check

--- routers/routers.go
+++ routers/routers.go
@@ -19,33 +19,37 @@ var mimeTypes = map[string]string{
 
 //goes inside the public folder and returens any resource of type not html
 func Public(res http.ResponseWriter, req *http.Request) {
-	logging.Request(req.RequestURI)
-	resToSend, extension, err := getResource.ResourcePT(req.URL.Path)
-	//if there is an error gettig resource return 404 and stop the response
-	if err != nil {
-		send.NotFound(res)
-		fmt.Println(err)
-		return
+	if req.Method == "GET" {
+		logging.Request(req.RequestURI)
+		resToSend, extension, err := getResource.ResourcePT(req.URL.Path)
+		//if there is an error gettig resource return 404 and stop the response
+		if err != nil {
+			send.NotFound(res)
+			fmt.Println(err)
+			return
+		}
+		res.Header().Set("content-type", mimeTypes[extension])
+		res.WriteHeader(http.StatusOK)
+		res.Write(resToSend)
 	}
-	res.Header().Set("content-type", mimeTypes[extension])
-	res.WriteHeader(http.StatusOK)
-	res.Write(resToSend)
 }
 
 //respinsible for / html
 func Default(res http.ResponseWriter, req *http.Request) {
-	//log the request
-	logging.Request(req.RequestURI)
-	//get the correct HTML Page. will stop here and return 404 if html is not found
-	htmlPage := getResource.HTMLPT("home", res)
-	//exit function because there was an error in finding html so 4040 was sent
-	if htmlPage == nil {
-		return
+	if req.Method == "GET" {
+		//log the request
+		logging.Request(req.RequestURI)
+		//get the correct HTML Page. will stop here and return 404 if html is not found
+		htmlPage := getResource.HTMLPT("home", res)
+		//exit function because there was an error in finding html so 4040 was sent
+		if htmlPage == nil {
+			return
+		}
+		//set the status to ok
+		res.WriteHeader(http.StatusOK)
+		//send the page
+		res.Write(htmlPage)
 	}
-	//set the status to ok
-	res.WriteHeader(http.StatusOK)
-	//send the page
-	res.Write(htmlPage)
 }
 
 //this is responsible for the handling of /delivery and /delivery/login
@@ -64,8 +68,10 @@ func Settings(res http.ResponseWriter, req *http.Request) {
 
 //this is responsible for handling pt html requests
 func PT(res http.ResponseWriter, req *http.Request) {
-	logging.Request(req.RequestURI)
-	HTMLPage := getResource.HTMLPT(req.URL.Path, res)
-	res.WriteHeader(http.StatusOK)
-	res.Write(HTMLPage)
+	if req.Method == "GET" {
+		logging.Request(req.RequestURI)
+		HTMLPage := getResource.HTMLPT(req.URL.Path, res)
+		res.WriteHeader(http.StatusOK)
+		res.Write(HTMLPage)
+	}
 }
