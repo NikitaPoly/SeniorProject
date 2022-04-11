0cb7b1683811d97fd0b44fa174ea7b5a11b69e9d
nikitapoly <nikitapoly@comcast.net>
2022-04-11 18:55:40 -0400

    testing db for new users

---
+++ getdb/getdb.go
@@ -0,0 +1,46 @@
+package getdb
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const mongoURI = "mongodb+srv://PolyakovDOTTech:Secure@polyakovtechdb.n6fvv.mongodb.net/PolyakovTechDB?retryWrites=true&w=majority"
+
+func GetUserInfo(GUserID string) map[string]string {
+	//get client for mongodb
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	//connext to mongodb using client with 10 second timeout limit
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	if err = client.Connect(ctx); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer client.Disconnect(ctx)
+
+	PolyakovTechDB := client.Database("PolyakovTechDB")
+	DB := PolyakovTechDB.Collection("DeliveryUsers")
+
+	cursor, err := DB.Find(ctx, bson.M{"DeliveryID": GUserID})
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	var userDataFinal []bson.M
+	if err = cursor.All(ctx, &userDataFinal); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	fmt.Println(userDataFinal)
+	return nil
+}
--- logic/logic.go
+++ logic/logic.go
@@ -1,13 +1,16 @@
 package logic
 
 import (
+	"Server/getdb"
 	"Server/savedb"
+	"Server/send"
 	"net/http"
 )
 
 //return true if user id is already saved, otherwise false
 func checkIfUserExists(userID string) bool {
-	return false
+	user := getdb.GetUserInfo(userID)
+	return user == nil
 }
 
 //logs in or creates new user for login page
@@ -18,7 +21,9 @@ func LoginOrSignupUser(res http.ResponseWriter, req *http.Request) {
 		userData[key] = value[0]
 	}
 	userExistAlready := checkIfUserExists(userData["credential"])
+	//if user does not exist save the id to db
 	if !userExistAlready {
 		savedb.CreateNewUser(res, userData["credential"])
 	}
+	send.SendDLogin(res)
 }
