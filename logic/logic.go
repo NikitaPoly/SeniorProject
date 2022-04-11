51a0e1999d137522f9c034806672280bf93e4340
nikitapoly <nikitapoly@comcast.net>
2022-04-11 18:00:04 -0400

    t

--- logic/logic.go
+++ logic/logic.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 )
 
+func checkIfUserExists(userID string) bool {
+	return false
+}
+
 func LoginOrSignupUser(res http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
 	userData := make(map[string]string)
 	for key, value := range req.Form {
 		userData[key] = value[0]
 	}
-	fmt.Println(userData)
+	//userExistAlready := checkIfUserExists(userData["credential"])
+	fmt.Println(userData["credential"])
 }
